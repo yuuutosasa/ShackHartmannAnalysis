e8e81fe03e29ab55c77d90cc57c11b69f3a85867
Yuto Sasaki <abbbbk4532sn@g.ecc.u-tokyo.ac.jp>
2024-06-10 15:05:10 +0900

    Add explanation to main

--- main.py
+++ main.py
@@ -12,17 +12,19 @@ from lib.shifts import *
 # MAIN CODE
 # -----------------------------------------------------------------------------
 
+# Read example image
 img_shifts=cv2.imread("example_image.jpg")
 
+# Output example image with subaperture grids with the center
 img_grid=draw_grid(img_shifts)
 plt.imshow(img_grid)
 plt.savefig("grid.png")
 
+# Prepare a grayscale image
 img_gray=img_shifts[...,0]
 img_gray=cut2zero(img_gray)
 
 # Compute shifts
-
 shifts=shifts(img_gray)
 np.save("shifts_array.npy", shifts)
 print(shifts[0,:])
